209b3b5b6d758592fccff5318c21b221b1fc472c
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-05-19 15:48:49 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/TweetTest">
+    <BrowserRouter basename="/Tweet">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
