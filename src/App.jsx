d74a0daafbc695a561b106c1640c601bee64d35f
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-05-19 19:37:54 +0300

    delete some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Suspense, lazy } from 'react';
-import { Route, Routes, redirect } from 'react-router-dom';
+import { Route, Routes, redirect, } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 
 const HomePages = lazy(() => import('./pages/HomePage/HomePage'));
 const TweetPage = lazy(() => import('./pages/TweetPage/TweetPage'));
-const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'))
 
 export const App = () => {
   return (
@@ -17,7 +16,7 @@ export const App = () => {
         <Route path="/" element={<HomePages />}/>
         <Route path="/tweets" element={<TweetPage />} />
           
-        <Route path='*' element={<HomePages/>} />
+          <Route path='*' element={redirect}/>
       </Routes>
       </Suspense>
       <Footer/>
