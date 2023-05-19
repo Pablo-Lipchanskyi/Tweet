36e02e62dfed6d75f73e5a390e9356325564df7e
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-05-19 19:42:04 +0300

    vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Suspense, lazy } from 'react';
-import { Route, Routes, redirect, } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 
 const HomePages = lazy(() => import('./pages/HomePage/HomePage'));
 const TweetPage = lazy(() => import('./pages/TweetPage/TweetPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'))
 
 export const App = () => {
   return (
@@ -15,8 +16,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<HomePages />}/>
         <Route path="/tweets" element={<TweetPage />} />
-          
-          <Route path='*' element={redirect}/>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       </Suspense>
       <Footer/>
